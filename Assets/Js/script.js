02fc30271a54fd7f456f4c2b6590b2d68d1b2b44
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2023-07-15 19:24:58 +0530

    PortFolio, Update Profile i.e. change Profile pics, add typed text and with responsive

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -1,1 +1,8 @@
 console.log("I'm Web Developer ROHIT")
+
+var typed = new Typed('#typed-text', {
+    strings: ['Front-End Developer', 'Web Coder', 'Web Programmer', 'Wedsite Development Learner'],
+    typeSpeed: 90,
+    backSpeed: 70,
+    loop: true
+  });
