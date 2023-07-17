25f9cd24ba284fdd3415ac39e31a3c77abe6ffaa
Rohit Nittawadekar <rohitnittawadekar07@gmail.com>
2023-07-17 08:59:16 +0530

    PortFolio, main div converting as section Tag, profile are responsive in largeer break point, And correct spalling mmistakes

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -1,7 +1,7 @@
 console.log("I'm Web Developer ROHIT")
 
 var typed = new Typed('#typed-text', {
-    strings: ['Front-End Developer', 'Web Coder', 'Web Programmer', 'Wedsite Development Learner'],
+    strings: ['Front-End Developer', 'Web Coder', 'Web Programmer', 'Website Development Learner'],
     typeSpeed: 90,
     backSpeed: 70,
     loop: true
